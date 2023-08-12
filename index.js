20368d88d0adcff009fa4abaca4f30ad73e9205b
Thite Shubham <115460300+shubhamthite09@users.noreply.github.com>
2023-08-12 14:18:58 +0530

    new added

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get("/",(req, res) => {
     }
 });
 
+app.get("/pk",(req, res) => {
+    try{
+        res.status(200).json({isError:false,msg:"pankaj is chutiya"});
+    }catch(err){
+        res.status(404).json({isError:true,msg:err.massage});
+    }
+});
+
 app.listen(8900,()=>{
     console.log("running test server on " + 8900);
 })
