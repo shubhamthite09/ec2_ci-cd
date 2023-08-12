2db489308455997d0c33c748f954fd92ba35953c
Thite Shubham <115460300+shubhamthite09@users.noreply.github.com>
2023-08-12 12:46:16 +0530

    inital

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req, res) => {
+    try{
+        res.status(200).json({isError:false,msg:"ok"});
+    }catch(err){
+        res.status(404).json({isError:true,msg:err.massage});
+    }
+});
+
+app.listen(8900,()=>{
+    console.log("running test server on " + 8900);
+})
